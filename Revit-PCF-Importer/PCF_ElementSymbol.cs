64ac225578dab6bf9ba14e3a598dc19834ab1b4c
Michail Golubjev <xopkop@gmail.com>
2016-07-17 01:51:55 +0200

    Paaaaaarsing

---
+++ Revit-PCF-Importer/PCF_ElementSymbol.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revit_PCF_Importer
+{
+    public class ElementSymbol
+    {
+        public string ElementType { get; set; } //The element type.
+        public StringCollection SourceData { get; set; } //This contains the raw data read from file
+        public string PipelineReference { get; set; } //This contains the pipeline reference that was read
+    }
+}
--- Revit-PCF-Importer/PCF_Main.cs
+++ Revit-PCF-Importer/PCF_Main.cs
@@ -1,13 +1,16 @@
 #region Namespaces
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 
@@ -21,6 +24,11 @@ namespace Revit_PCF_Importer
     [Regeneration(RegenerationOption.Manual)]
     public class PCFImport : IExternalCommand
     {
+        //Declare the element collector
+        public ElementSymbol[] ElementCollector;
+        public ElementSymbol CurElementSymbol;
+        public StringCollection[] SourceData;
+
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
         {
             UIApplication uiapp = commandData.Application;
@@ -31,32 +39,53 @@ namespace Revit_PCF_Importer
             //Read the input file
             FileReader fileReader = new FileReader();
             string[] readFile = fileReader.ReadFile();
-            //Instantiate the PCF_Dictionary mega complex refactoring switch solution which doesn't really work
-            PCF_Dictionary dictionary = new PCF_Dictionary(new KeywordProcessor());
+
+            StringCollection resultList;
+
+            //Iteration counter
+            int iterationCounter = -1;
+            //Element index
+            int curElementIndex = 0;
+            
             foreach (string line in readFile)
             {
-                //Execute keyword handling
-                //Declare a StringCollection to hold the matches
-                StringCollection resultList = new StringCollection();
-                //Define a Regex to parse the input
-                Regex parseWords = new Regex(@"(\S+)");
-                //Define a Match to handle the results from Regex
-                Match match = parseWords.Match(line);
-                //Add every match from Regex to the StringCollection
-                while (match.Success)
+                //Count iterations
+                iterationCounter++;
+
+                //Logic test for Type or Property
+                if (!line.StartsWith("    "))
                 {
-                    //Only add the result if it is not a white space or null
-                    if (!string.IsNullOrEmpty(match.Value)) resultList.Add(match.Value);
-                    match = match.NextMatch();
+                    curElementIndex = iterationCounter;
+                    CurElementSymbol = new ElementSymbol();
                 }
-                //Separate the keyword and the rest of words from the results
-                string keyword = resultList[0];
-                //Remove the keyword from the results
-                resultList.RemoveAt(0);
-                //Parse keywords
-                var returns = dictionary.ParseKeywords(keyword, resultList);
+
+                Idea: Parse the whole file and mark the position of pipeline references.
+
+                ////Execute keyword handling
+                ////Declare a StringCollection to hold the matches
+                //StringCollection resultList = new StringCollection();
+
+                ////Define a Regex to parse the input
+                //Regex parseWords = new Regex(@"(\S+)");
+
+                ////Define a Match to handle the results from Regex
+                //Match match = parseWords.Match(line);
+
+                ////Add every match from Regex to the StringCollection
+                //while (match.Success)
+                //{
+                //    //Only add the result if it is not a white space or null
+                //    if (!string.IsNullOrEmpty(match.Value)) resultList.Add(match.Value);
+                //    match = match.NextMatch();
+                //}
+                ////Separate the keyword and the rest of words from the results
+                //string keyword = resultList[0];
+                ////Remove the keyword from the results
+                //resultList.RemoveAt(0);
+                //////Parse keywords
+                ////var returns = dictionary.ParseKeywords(keyword, resultList);
             }
-            
+
             //using (Transaction tx = new Transaction(doc))
             //{
             //    tx.Start("Transaction Name");
